e5252b53fbee47a24006b1df14b5508172e1418a
baizz <baizz1@163.com>
2014-07-25 13:28:21 +0800

    [develop]账户分析DAO

---
+++ perfect-dao/src/main/java/com/perfect/dao/AccountAnalyzeDAO.java
@@ -0,0 +1,9 @@
+package com.perfect.dao;
+
+import com.perfect.entity.KeywordRealTimeDataVOEntity;
+
+/**
+ * Created by baizz on 14-7-25.
+ */
+public interface AccountAnalyzeDAO extends CrudRepository<KeywordRealTimeDataVOEntity, Long> {
+}
---
+++ perfect-dao/src/main/java/com/perfect/mongodb/dao/impl/AccountAnalyzeDAOImpl.java
@@ -0,0 +1,61 @@
+package com.perfect.mongodb.dao.impl;
+
+import com.perfect.dao.AccountAnalyzeDAO;
+import com.perfect.entity.KeywordRealTimeDataVOEntity;
+import com.perfect.mongodb.utils.BaseMongoTemplate;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by baizz on 14-7-25.
+ */
+public class AccountAnalyzeDAOImpl implements AccountAnalyzeDAO {
+
+    private MongoTemplate mongoTemplate = BaseMongoTemplate.getMongoTemplate("perfect");
+
+    public KeywordRealTimeDataVOEntity findOne(Long aLong) {
+        return null;
+    }
+
+    public List<KeywordRealTimeDataVOEntity> findAll() {
+        return null;
+    }
+
+    public List<KeywordRealTimeDataVOEntity> find(Map<String, Object> params, int skip, int limit) {
+        return null;
+    }
+
+    public void insert(KeywordRealTimeDataVOEntity keywordRealTimeDataVOEntity) {
+
+    }
+
+    public void insertAll(List<KeywordRealTimeDataVOEntity> entities) {
+
+    }
+
+    public void update(KeywordRealTimeDataVOEntity keywordRealTimeDataVOEntity) {
+
+    }
+
+    public void update(List<KeywordRealTimeDataVOEntity> entities) {
+
+    }
+
+    public void deleteById(Long aLong) {
+
+    }
+
+    public void deleteByIds(List<Long> longs) {
+
+    }
+
+    public void delete(KeywordRealTimeDataVOEntity keywordRealTimeDataVOEntity) {
+
+    }
+
+    public void deleteAll() {
+
+    }
+}
