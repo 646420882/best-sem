da229f1852f691248052f65e4a376d4263bf214a
ShuZiDong <shuzichao@126.com>
2014-07-28 14:34:17 +0800

    [account] 对用户数据查询以及新建用户实体

--- perfect-dao/src/main/java/com/perfect/dao/AccountAnalyzeDAO.java
+++ perfect-dao/src/main/java/com/perfect/dao/AccountAnalyzeDAO.java
@@ -1,9 +1,16 @@
 package com.perfect.dao;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.perfect.entity.AccountAnalyzeEntity;
 import com.perfect.entity.KeywordRealTimeDataVOEntity;
+
+import java.util.List;
+
 
 /**
  * Created by baizz on 14-7-25.
  */
 public interface AccountAnalyzeDAO extends CrudRepository<KeywordRealTimeDataVOEntity, Long> {
+    List<AccountAnalyzeEntity> performance(String userTable);
 }
