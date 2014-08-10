8d7e2d8f5766b450a5ee65e03f911f1db6daee21
yousheng <vbzerocool@gmail.com>
2014-08-10 15:47:11 +0800

    [scheudleTask] for merging

--- perfect-scheduler/src/main/java/com/perfect/schedule/task/execute/ReportDataTask.java
+++ perfect-scheduler/src/main/java/com/perfect/schedule/task/execute/ReportDataTask.java
@@ -1,9 +1,17 @@
 package com.perfect.schedule.task.execute;
 
+import com.perfect.api.baidu.RealTimeDataReports;
+import com.perfect.autosdk.core.ServiceFactory;
+import com.perfect.autosdk.sms.v3.RealTimeResultType;
+import com.perfect.dao.SystemUserDAO;
+import com.perfect.entity.BaiduAccountInfoEntity;
+import com.perfect.entity.SystemUserEntity;
 import com.perfect.schedule.core.IScheduleTaskDealSingle;
 import com.perfect.schedule.task.conf.TaskConfig;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -14,12 +22,35 @@ import java.util.List;
  * @author yousheng
  */
 @Component("reportDataTask")
-public class ReportDataTask implements IScheduleTaskDealSingle{
+public class ReportDataTask implements IScheduleTaskDealSingle<SystemUserEntity> {
 
+    @Resource
+    private MongoTemplate mongoTemplate;
+
+    @Resource
+    private SystemUserDAO systemUserDAO;
 
 
     @Override
-    public boolean execute(Object task, String ownSign) throws Exception {
+    public boolean execute(SystemUserEntity task, String ownSign) throws Exception {
+
+        List<BaiduAccountInfoEntity> baiduAccountInfoEntityList = task.getBaiduAccountInfoEntities();
+        List<RealTimeResultType> realTimeResultTypeList = RealTimeDataReports.getUnitRealTimeDataPC(null, null);
+        String dbname = "user_" + task.getUserName() + "_report";
+
+        for (BaiduAccountInfoEntity entity : baiduAccountInfoEntityList) {
+            String tokenId = entity.getToken();
+            String username = entity.getBaiduUserName();
+            String pwd = entity.getBaiduPassword();
+
+            ServiceFactory sf = ServiceFactory.getInstance(username, pwd
+                    , tokenId, null);
+
+
+        }
+
+
+        mongoTemplate.create
         return false;
     }
 
@@ -40,9 +71,9 @@ public class ReportDataTask implements IScheduleTaskDealSingle{
             return Collections.emptyList();
         }
 
+        List<SystemUserEntity> userEntityList = systemUserDAO.findAll();
 
-
-        return null;
+        return userEntityList;
     }
 
     @Override
