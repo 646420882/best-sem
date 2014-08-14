715e7c13318b4ddbf7a5b37200c5930eec7a6600
XieJiao <xiejiao@perfect-cn.cn>
2014-08-14 16:25:42 +0800

    [accountWarning]update

--- perfect/src/main/java/com/perfect/app/transmitter/Sender.java
+++ perfect/src/main/java/com/perfect/app/transmitter/Sender.java
@@ -18,22 +18,3 @@ public class Sender implements Runnable{
     }
 
 }
-
-
-/*
-
-//这个类主要是设置邮件
-MailSenderInfo mailInfo = new MailSenderInfo();
-mailInfo.setMailServerHost("smtp.163.com");
-        mailInfo.setMailServerPort("25");
-        mailInfo.setValidate(true);
-        mailInfo.setUserName("15923844052@163.com");
-        mailInfo.setPassword("xj691864758xk.");//您的邮箱密码
-        mailInfo.setFromAddress("15923844052@163.com");
-        mailInfo.setToAddress("691864758@qq.com");
-        mailInfo.setSubject("我的测试邮件");
-        mailInfo.setContent("邮件可以了!");
-        //这个类主要来发送邮件
-        SimpleMailSender sms = new SimpleMailSender();
-        sms.sendTextMail(mailInfo);//发送文体格式
-        sms.sendHtmlMail(mailInfo);//发送html格式*/
