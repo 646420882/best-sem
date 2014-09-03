67f7c59df160591c0a748a9d5539812e93c8cbaa
XieJiao <xiejiao@perfect-cn.cn>
2014-09-03 09:44:14 +0800

    [promotionAssistant]调度任务update

---
+++ perfect-scheduler/src/main/java/com/perfect/schedule/task/conf/impl/DataPullTaskConfig.java
@@ -0,0 +1,24 @@
+package com.perfect.schedule.task.conf.impl;
+
+import com.perfect.schedule.core.taskmanager.ScheduleTaskType;
+
+/**
+ * Created by john on 2014/9/2.
+ */
+public class DataPullTaskConfig extends BaseTaskConfig{
+
+    public DataPullTaskConfig(String baseTaskName, String ownSig, String dealBean, String cronExp, String[] taskDef) {
+        super(baseTaskName, ownSig, dealBean, cronExp, taskDef);
+    }
+
+
+    @Override
+    protected ScheduleTaskType createTaskType() {
+        ScheduleTaskType scheduleTaskType = super.createTaskType();
+        scheduleTaskType.setSleepTimeInterval(1000*60*60*24);
+        scheduleTaskType.setPermitRunEndTime(null);
+
+        return scheduleTaskType;
+    }
+
+}
