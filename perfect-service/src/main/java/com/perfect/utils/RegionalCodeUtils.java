8fff4ebfc63d67d6fad402f8e276a3d2b0b3db59
SubDong <shuzichao@126.com>
2014-09-17 18:03:05 +0800

    [Region] 跟新地域名称反查地域id功能

--- perfect-service/src/main/java/com/perfect/utils/RegionalCodeUtils.java
+++ perfect-service/src/main/java/com/perfect/utils/RegionalCodeUtils.java
@@ -2,9 +2,11 @@ package com.perfect.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.perfect.entity.StructureReportEntity;
 import com.perfect.redis.JRedisUtils;
 import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +23,22 @@ public class RegionalCodeUtils {
         Map<String,String> d = gson.fromJson(data, new TypeToken<Map<Long,String>>() {}.getType());
         Map<Long,String> map = new HashMap<>();
         for (Long aLong : id){
-            map.put(aLong,d.get(aLong));
+            map.put(aLong, d.get(aLong));
+        }
+        return map;
+    }
+    public static  Map<Long,String> regionalCodeName(List<String> name){
+        Jedis jc = JRedisUtils.get();
+        String data = jc.get("RegionalCodeRedis");
+        Gson gson = new Gson();
+        Map<Long,String> d = gson.fromJson(data, new TypeToken<Map<Long,String>>() {}.getType());
+        Map<Long,String> map = new HashMap<>();
+        for (String aString : name){
+            for (Map.Entry<Long, String> voEntity : d.entrySet()){
+                if(aString.equals(voEntity.getValue())){
+                    map.put(voEntity.getKey(),aString);
+                }
+            }
         }
         return map;
     }
