8b1743d1ace309611da72bbfef214acbbf2af2fd
baizz <baizz1@163.com>
2014-09-17 22:07:27 +0800

    [develop] bug ,fix1

--- perfect-service/src/main/java/com/perfect/service/keywordReportService.java
+++ perfect-service/src/main/java/com/perfect/service/keywordReportService.java
@@ -3,11 +3,12 @@ package com.perfect.service;
 import com.perfect.dao.MongoCrudRepository;
 import com.perfect.entity.KeywordReportEntity;
 import com.perfect.mongodb.utils.PagerInfo;
+
 import java.util.Map;
 
 /**
- * Created by XiaoWei on 2014/9/17.
+ * Created by XiaoWei on 2014-9-17.
  */
-public interface KeywordReportService extends MongoCrudRepository<KeywordReportEntity,Long>{
-    PagerInfo findByPagerInfo(Map<String,Object> params);
+public interface KeywordReportService extends MongoCrudRepository<KeywordReportEntity, Long> {
+    PagerInfo findByPagerInfo(Map<String, Object> params);
 }
