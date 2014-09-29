3269b8344ccbfdaabab9e6f6d2b1212b74bd7c24
XiaoWei <xiaowei@perfect-cn.cn>
2014-09-29 09:53:30 +0800

    [add] 编辑创意

---
+++ perfect/src/main/java/com/perfect/app/homePage/controller/ExtController.java
@@ -0,0 +1,7 @@
+package com.perfect.app.homePage.controller;
+
+/**
+ * Created by XiaoWei on 2014/9/26.
+ */
+public class ExtController {
+}
---
+++ perfect/src/main/webapp/public/js/assistant/tuijianCreative.js
@@ -0,0 +1,3 @@
+/**
+ * Created by XiaoWei on 2014/9/28.
+ */
