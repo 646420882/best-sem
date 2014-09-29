c49a2eb22c28e2491c9453eba7c6501980f295d3
SubDong <shuzichao@126.com>
2014-09-29 17:33:39 +0800

    [Regional] 重构低于编码更能以及添加到数据库

--- perfect-service/src/main/java/com/perfect/utils/RegionalCodeUtils.java
+++ perfect-service/src/main/java/com/perfect/utils/RegionalCodeUtils.java
@@ -2,8 +2,7 @@ package com.perfect.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.perfect.dto.AccountReportDTO;
-import com.perfect.dto.RedisRegional;
+import com.perfect.dto.RegionalCodeDTO;
 import com.perfect.redis.JRedisUtils;
 import redis.clients.jedis.Jedis;
 
@@ -72,28 +71,28 @@ public class RegionalCodeUtils {
      * @param id
      * @return
      */
-    public static Map<Integer, List<RedisRegional>> provinceCode(List<Integer> id) {
+    public static Map<Integer, List<RegionalCodeDTO>> provinceCode(List<Integer> id) {
         Jedis jc = JRedisUtils.get();
         String data = jc.get("RegionalCodeRedis");
         Gson gson = new Gson();
         Map<Integer, String> mapKey = gson.fromJson(data, new TypeToken<Map<Integer, String>>() {
         }.getType());
 
-        Map<Integer, List<RedisRegional>> map = new HashMap<>();
+        Map<Integer, List<RegionalCodeDTO>> map = new HashMap<>();
 
         for (Integer integer : id) {
 
-            List<RedisRegional> redisRegionalList = new ArrayList<>();
+            List<RegionalCodeDTO> redisRegionalList = new ArrayList<>();
 
             for (Map.Entry<Integer, String> voEntity : mapKey.entrySet()) {
 
-                RedisRegional redisRegional = new RedisRegional();
+                RegionalCodeDTO redisRegional = new RegionalCodeDTO();
                 int regional = voEntity.getKey() / 1000;
                 int proCode = integer / 1000;
                 if (regional == proCode) {
                     if (voEntity.getKey().intValue() != integer.intValue()) {
-                        redisRegional.setRegionalId(voEntity.getKey());
-                        redisRegional.setRegionalName(voEntity.getValue());
+                      /*  redisRegional.setRegionalId(voEntity.getKey());
+                        redisRegional.setRegionalName(voEntity.getValue());*/
                         redisRegionalList.add(redisRegional);
                     }
                 }
