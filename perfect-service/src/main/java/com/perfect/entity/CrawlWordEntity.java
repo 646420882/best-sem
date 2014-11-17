477bec06e94d85f88b99f4d6051ee6c15683e5d1
baizz <baizz1@163.com>
2014-11-17 15:15:55 +0800

    [develop] pom文件修改

---
+++ perfect-service/src/main/java/com/perfect/entity/CrawlWordEntity.java
@@ -0,0 +1,66 @@
+package com.perfect.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+/**
+ * Created by baizz on 2014-11-17.
+ */
+@Document(collection = "sys_crawl_word")
+public class CrawlWordEntity {
+
+    @Id
+    private String id;
+
+    private String site;
+
+    @Field("c")
+    private String category;
+
+    @Field("w")
+    private String keyword;
+
+    @Field("s")
+    private Integer status = 0;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
