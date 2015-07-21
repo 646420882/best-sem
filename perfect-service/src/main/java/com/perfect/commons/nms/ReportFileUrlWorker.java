07b1110fe739bd0e557c4a7742b8aa0385e8296a
dolphineor <dolphineor@gmail.com>
2015-07-21 14:33:28 +0800

    网盟推广Api更新

---
+++ perfect-service/src/main/java/com/perfect/commons/nms/ReportFileUrlWorker.java
@@ -0,0 +1,11 @@
+package com.perfect.commons.nms;
+
+/**
+ * Created by dolphineor on 2015-7-21.
+ */
+public class ReportFileUrlWorker implements Runnable {
+    @Override
+    public void run() {
+        System.out.println();
+    }
+}
--- perfect-service/src/main/java/com/perfect/service/impl/AsynchronousNmsReportServiceImpl.java
+++ perfect-service/src/main/java/com/perfect/service/impl/AsynchronousNmsReportServiceImpl.java
@@ -5,9 +5,16 @@ import com.perfect.dto.account.NmsAdReportDTO;
 import com.perfect.dto.account.NmsCampaignReportDTO;
 import com.perfect.dto.account.NmsGroupReportDTO;
 import com.perfect.service.AsynchronousNmsReportService;
+import com.perfect.utils.redis.JRedisUtils;
+import redis.clients.jedis.Jedis;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static com.perfect.commons.constants.RedisConstants.REPORT_FILE_URL_SUCCEED;
+import static com.perfect.commons.constants.RedisConstants.REPORT_ID_COMMIT_STATUS;
 
 /**
  * Created by subdong on 15-7-21.
@@ -36,6 +43,60 @@ public class AsynchronousNmsReportServiceImpl implements AsynchronousNmsReportSe
 
 
     class HttpFileHandler {
+
+        private BlockingQueue<String> accountQueue = new LinkedBlockingQueue<>();
+        private BlockingQueue<String> campaignQueue = new LinkedBlockingQueue<>();
+        private BlockingQueue<String> groupQueue = new LinkedBlockingQueue<>();
+        private BlockingQueue<String> adQueue = new LinkedBlockingQueue<>();
+
+
+        public HttpFileHandler() {
+            init();
+        }
+
+        private void init() {
+            while (true) {
+                Jedis jedis = null;
+                try {
+                    jedis = JRedisUtils.get();
+                    String value = jedis.rpop(REPORT_FILE_URL_SUCCEED);
+                    String status = jedis.get(REPORT_ID_COMMIT_STATUS);
+                    if (value == null && "1".equals(status)) {
+                        jedis.close();
+                        break;
+                    }
+
+                    if (value == null) {
+                        jedis.close();
+                        continue;
+                    }
+
+                    int type = Integer.parseInt(value.split("\\|")[0]);
+                    String fileUrl = value.split("\\|")[1];
+
+                    switch (type) {
+                        case 1:
+                            accountQueue.add(fileUrl);
+                            break;
+                        case 2:
+                            campaignQueue.add(fileUrl);
+                            break;
+                        case 3:
+                            groupQueue.add(fileUrl);
+                            break;
+                        case 4:
+                            adQueue.add(fileUrl);
+                            break;
+                        default:
+                            break;
+                    }
+
+                } finally {
+                    if (jedis != null)
+                        jedis.close();
+                }
+            }
+        }
 
         public List<NmsAccountReportDTO> getNmsAccountReport(String fileUrl) {
             return Collections.emptyList();
