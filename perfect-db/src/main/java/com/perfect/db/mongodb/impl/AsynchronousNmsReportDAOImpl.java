0d5d5ec450ecfadf782d2ad85198ed2b43e8e1ec
subdong <shuzichao@126.com>
2015-07-22 14:59:54 +0800

    [add] 网盟推广dao层

--- perfect-db/src/main/java/com/perfect/db/mongodb/impl/AsynchronousNmsReportDAOImpl.java
+++ perfect-db/src/main/java/com/perfect/db/mongodb/impl/AsynchronousNmsReportDAOImpl.java
@@ -2,14 +2,26 @@ package com.perfect.db.mongodb.impl;
 
 import com.perfect.dao.report.AsynchronousNmsReportDAO;
 import com.perfect.db.mongodb.base.AbstractUserBaseDAOImpl;
+import com.perfect.db.mongodb.base.BaseMongoTemplate;
 import com.perfect.dto.SystemUserDTO;
 import com.perfect.dto.account.NmsAccountReportDTO;
 import com.perfect.dto.account.NmsAdReportDTO;
 import com.perfect.dto.account.NmsCampaignReportDTO;
 import com.perfect.dto.account.NmsGroupReportDTO;
 import com.perfect.entity.report.NmsAccountReportEntity;
+import com.perfect.entity.report.NmsCampaignReportEntity;
+import com.perfect.entity.report.NmsGroupReportEntity;
+import com.perfect.utils.ObjectUtils;
+import com.perfect.utils.mongodb.DBNameUtils;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,22 +31,81 @@ import java.util.List;
 public class AsynchronousNmsReportDAOImpl extends AbstractUserBaseDAOImpl<NmsAccountReportDTO, Long> implements AsynchronousNmsReportDAO {
     @Override
     public void getNmsAccountReportData(List<NmsAccountReportDTO> nmsAccountReportDtos, SystemUserDTO systemUser, String dateStr, String baiduUserName) {
+        MongoTemplate mongoTemplate;
+        mongoTemplate = BaseMongoTemplate.getMongoTemplate(DBNameUtils.getReportDBName(systemUser.getUserName()));
 
+        List<NmsAccountReportEntity> accountReportEntities = ObjectUtils.convert(nmsAccountReportDtos, NmsAccountReportEntity.class);
+        Date date = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = df.format(date);
+
+        List<NmsAccountReportEntity> entities = null;
+        if (!dateStr.equals(dateString)) {
+            try {
+                entities = mongoTemplate.find(Query.query(Criteria.where("date").is(new SimpleDateFormat("yyyy-MM-dd").parse(dateStr)).and("acna").is(baiduUserName)), NmsAccountReportEntity.class);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            if (entities == null || entities.size() == 0) {
+                mongoTemplate.insert(accountReportEntities, TBL_NMS_ACCOUNT_REPORT);
+            }
+        }
     }
 
     @Override
     public void getNmsCampaignReportData(List<NmsCampaignReportDTO> nmsCampaignReportDTOs, SystemUserDTO systemUser, String dateStr, int i) {
+        MongoTemplate mongoTemplate;
+        mongoTemplate = BaseMongoTemplate.getMongoTemplate(DBNameUtils.getReportDBName(systemUser.getUserName()));
+
+        List<NmsCampaignReportEntity> campaignReportEntities = new ArrayList<>(ObjectUtils.convert(nmsCampaignReportDTOs, NmsCampaignReportEntity.class));
+        List<NmsCampaignReportEntity> campaignReportEntities1;
+        if (mongoTemplate.collectionExists(dateStr + "-nms-campaign")) {
+            if (i > 1) {
+                campaignReportEntities1 = new ArrayList<>(mongoTemplate.find(new Query(), NmsCampaignReportEntity.class, dateStr + "-campaign"));
+                campaignReportEntities.addAll(campaignReportEntities1);
+            }
+            mongoTemplate.dropCollection(dateStr + "-nms-campaign");
+        }
+
+        mongoTemplate.insert(campaignReportEntities, dateStr + "-nms-campaign");
 
     }
 
     @Override
     public void getNmsGroupReportData(List<NmsGroupReportDTO> nmsGroupReportDtos, SystemUserDTO systemUser, String dateStr, int i) {
+        MongoTemplate mongoTemplate;
+        mongoTemplate = BaseMongoTemplate.getMongoTemplate(DBNameUtils.getReportDBName(systemUser.getUserName()));
 
+        List<NmsGroupReportEntity> adgroupReportEntities = new ArrayList<>(ObjectUtils.convert(nmsGroupReportDtos, NmsGroupReportEntity.class));
+        List<NmsGroupReportEntity> adgroupReportEntities1;
+        if (mongoTemplate.collectionExists(dateStr + "-nms-adgroup")) {
+            if (i > 1) {
+                adgroupReportEntities1 = new ArrayList<>(mongoTemplate.find(new Query(), NmsGroupReportEntity.class, dateStr + "-nms-adgroup"));
+                adgroupReportEntities.addAll(adgroupReportEntities1);
+            }
+            mongoTemplate.dropCollection(dateStr + "-nms-adgroup");
+        }
+
+        mongoTemplate.insert(adgroupReportEntities, dateStr + "-nms-adgroup");
     }
 
     @Override
     public void getNmsAdReportData(List<NmsAdReportDTO> nmsAdReportDTOs, SystemUserDTO systemUser, String dateStr, int i) {
+        MongoTemplate mongoTemplate;
+        mongoTemplate = BaseMongoTemplate.getMongoTemplate(DBNameUtils.getReportDBName(systemUser.getUserName()));
 
+        List<NmsAccountReportEntity> creativeReportEntities = new ArrayList<>(ObjectUtils.convert(nmsAdReportDTOs, NmsAccountReportEntity.class));
+        List<NmsAccountReportEntity> creativeReportEntities1;
+        if (mongoTemplate.collectionExists(dateStr + "-nms-creative")) {
+            if (i > 1) {
+                creativeReportEntities1 = new ArrayList<>(mongoTemplate.find(new Query(), NmsAccountReportEntity.class, dateStr + "-creative"));
+                creativeReportEntities.addAll(creativeReportEntities1);
+            }
+            mongoTemplate.dropCollection(dateStr + "-nms-creative");
+        }
+
+        mongoTemplate.insert(creativeReportEntities, dateStr + "-nms-creative");
     }
 
     @Override
