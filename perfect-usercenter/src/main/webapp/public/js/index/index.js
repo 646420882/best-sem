d9806a657afa53e19ffdf56d0fca8c4c28fd7137
guochunyan <guochunyan@perfect-cn.cn>
2015-12-17 20:33:41 +0800

    [update]首页效果提交

--- perfect-usercenter/src/main/webapp/public/js/index/index.js
+++ perfect-usercenter/src/main/webapp/public/js/index/index.js
@@ -1,3 +1,35 @@
 /**
  * Created by guochunyan on 2015/12/17.
  */
+
+function Modify(_this) {
+    var list = $("#userList").find("b");
+    list.each(function () {
+        var listMessage = $(this).html();
+        $(this).html("<input type='text' class='form-control' style='display:inline-block;max-width:300px' value='" + listMessage + "'> ");
+        $(_this).hide();
+        $(_this).next().removeClass("hide");
+    })
+};
+function Preservation(_this) {
+    var list = $("#userList").find("input");
+    list.each(function () {
+        var listMessage = $(this).val();
+        console.log(listMessage);
+        $(this).replaceWith(listMessage);
+        $(_this).parent().prev().show();
+        $(_this).parent().addClass("hide");
+
+    })
+}
+function Cancel(_this) {
+    var list = $("#userList").find("input");
+    list.each(function () {
+            var listMessage = $(this).val();
+            $(this).replaceWith(listMessage);
+            $(_this).parent().prev().show();
+            $(_this).parent().addClass("hide");
+        }
+    )
+}
+
